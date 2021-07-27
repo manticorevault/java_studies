ab6f6425f33e65906f5119c83b00ae86ae1f271e
manticorevault <artur_seilah@hotmail.com>
2021-07-27 17:26:22 -0300

    Created some random program to name a cat

---
+++ src/FirstJava.java
@@ -0,0 +1,12 @@
+import model.cat_model;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		
+		cat_model kitty = new cat_model(null, null, null);
+
+		System.out.println(kitty);
+	}
+
+} 
